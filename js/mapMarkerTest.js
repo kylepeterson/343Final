64ec6d2f28bbded18b1065e0026ad6e0bd56fd0d
Jacob Grossman <jgrossman3643@gmail.com>
2013-12-04 11:56:54 -0800

    Map marker for one apartment so you can see the structure of the objects. Full list incoming

---
+++ js/mapMarkerTest.js
@@ -0,0 +1,12 @@
+var apartmentTest = {
+	list: [
+		{
+			name:'Cosmopolitan Apartments',
+			address:'400 NE 45th St',
+			city:'Seattle',
+			zip:'98105',
+			state:'WA',
+			reviews: []
+		}	
+	]
+}
