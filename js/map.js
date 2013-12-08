bc075526c9a1282cf777a9a1be30cf3eab8ed96d
unknown <iguest@is-m430m27-21.ischool.uw.edu>
2013-12-07 18:52:59 -0800

    Search implemented on map page

--- js/map.js
+++ js/map.js
@@ -3,19 +3,19 @@ var apartmentData = {};
 var map;
 
 $(function() {
-	makeMap(map);
+	makeMap();
 });
 
-function makeMap(map) {
+function makeMap() {
 		map = new google.maps.Map($('.map-container')[0], {
 		center: new google.maps.LatLng(47.662757, -122.314059),
 		zoom: 15,
 		minZoom: 15
 	});
-	addMarkers(map);
+	addMarkers();
 }
 
-function addMarkers(map) {
+function addMarkers() {
 	$.getJSON(url, function(apartmentData) {
 		for (var i = 0; i < apartmentData.length; i++) {
 			var apartment = apartmentData[i];
@@ -36,19 +36,24 @@ function addMarkers(map) {
 			//var pano = new google.maps.StreetViewPanorama(infoWindow, function(){
 				//position: new google.maps.LatLng(apartment.lat, apartment.lng)
 			//});
-			registerInfoWindow(map, marker, infoWindow);
+			registerInfoWindow(marker, infoWindow);
 		}
 	});
 }
 
 
-function registerInfoWindow(map, marker, infoWindow) {
+function registerInfoWindow(marker, infoWindow) {
 	google.maps.event.addListener(marker, 'click', function() {
 		if (apartmentData.iw) {
 			apartmentData.iw.close();
 		}
 		apartmentData.iw = infoWindow;
 		infoWindow.open(map,marker);
-		map.setCenter(this.getPosition());
+		map.panTo(this.getPosition());
 	});
+}
+
+function panWindow(apartment) {
+	map.panTo(new google.maps.LatLng(apartment.lat, apartment.lng));
+	map.setZoom(10);
 }
