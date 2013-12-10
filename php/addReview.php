9a20762062ade4e9cc8b03129dc125d17e06dbef
William Smyth May <wsmay1@uw.edu>
2013-12-09 20:50:11 -0800

    Commented stuff

--- php/addReview.php
+++ php/addReview.php
@@ -1,16 +1,27 @@
 <?php
-	$apartmentName = $_POST['name'];
+	//This file is the php code that allows users to submit a review on The Dawg House.
+	//Composed by William Smyth May.
+	$apartmentName = $_POST['aptName'];
 	$apartmentData = json_decode(file_get_contents('http://students.washington.edu/wsmay1/info343/final/js/apartments.json'));
+	
+	//Iterates over the json object to find the requested apartment name
 	foreach ($apartmentData as $apt) {
 		if ($apt -> name == $apartmentName) {
-			print('success');
+			$found = true; //boolean that specifies that the apartment was found.
 			$reviews = $apt -> reviews;
 			array_push($reviews, $_POST);
-			$yep = true;
-			$apartmentData[$apt -> index] -> reviews = $reviews;
-			json_encode($apartmentData);
-			file_put_contents('../js/apartments.json', $apartmentData);
-			//header('http://students.washington.edu/wsmay1/info343/final/index.html');
+			$index = $apt -> index;
+			$apartmentData[$apt -> index] -> reviews = $reviews; //adds the new review to the actual JSON object
+			$json = json_encode($apartmentData); //The version of php on our hosting does NOT support JSON_PRETTY_PRINT, so it is encoded in one line.
+			file_put_contents('../js/apartments.json', $json); //rewrites the JSON object to the JSON file.
+			header('Location: http://students.washington.edu/wsmay1/info343/final/thanks.html'); //redirect users to a page notifying them of their successful review
+			die();
 		}
+	}
+	
+	//If the apartment name specified by the user is not found, the user is redirected and alerted of the error
+	if(!$found) {
+		header('Location: http://students.washington.edu/wsmay1/info343/final/notFound.html');
+		die();
 	}
 ?>
