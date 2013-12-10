c0a71ced2e9258827dac5193b83910768fe455a1
Kyle Peterson <kylep9@uw.edu>
2013-12-09 22:06:10 -0800

    removed the search bar from index.html

--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,5 @@
+/* Redirects the user to the map submit page after a delay */
+
 $(function() {
 	var delay = 1000; //Your delay in milliseconds
 	setTimeout(function(){ window.location = "map.shtml"; }, delay);
--- js/redirectToSubmit.js
+++ js/redirectToSubmit.js
@@ -1,3 +1,5 @@
+/* Redirects the user to the review submit page after a delay */
+
 $(function() {
 	var delay = 1000; //Your delay in milliseconds
 	setTimeout(function(){ window.location = "review.shtml"; }, delay);
