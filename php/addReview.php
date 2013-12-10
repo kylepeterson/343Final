94ae7f0dba973f7e961fb9eac63a3198b307872e
William Smyth May <wsmay1@uw.edu>
2013-12-10 02:30:17 -0800

    average funciton working, fully commented.

--- php/addReview.php
+++ php/addReview.php
@@ -5,8 +5,7 @@
 	$apartmentData = json_decode(file_get_contents('http://students.washington.edu/wsmay1/info343/final/js/apartments.json'));
 	
 	//Iterates over the json object to find the requested apartment name
-	$total = 0;
-	$scores = 0;
+	$scores = array();
 	foreach ($apartmentData as $apt) {
 		if ($apt -> name == $apartmentName) {
 			$found = true; //boolean that specifies that the apartment was found.
@@ -15,14 +14,14 @@
 			$index = $apt -> index;
 			$apartmentData[$apt -> index] -> reviews = $reviews; //adds the new review to the actual JSON object
 			
-			foreach ($reviews as $review) {
-				$scores += $review -> rating;
-				$total++;
+			//Calculates the average score of the apartment with the new review.
+			foreach($reviews as $review) {
+				array_push($scores, $review -> rating);
 			}
-			$avg = $scores / $total;
-			$apartmentData[$apt -> index] -> avg = $avg;
+			$avg = (array_sum($scores) + $_POST['rating']) / count($scores);
+			$apartmentData[$apt -> index] -> avg = $avg; //assigning the average score to the JSON object.
 			
-			$json = json_encode($apartmentData); //The version of php on our hosting does NOT support JSON_PRETTY_PRINT, so it is encoded in one line.
+			$json = json_encode($apartmentData); //The version of php on our hosting does NOT support JSON_PRETTY_PRINT, so it is encoded on one line.
 			file_put_contents('../js/apartments.json', $json); //rewrites the JSON object to the JSON file.
 			header('Location: http://students.washington.edu/wsmay1/info343/final/success.html'); //redirect users to a page notifying them of their successful review
 			die();
